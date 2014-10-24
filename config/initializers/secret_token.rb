0d0966217cd37c5aa8a47d669d33c3893998eef2
gayusaarathy <gayuceg@gmail.com>
2014-10-23 23:15:30 -0300

    Framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knowyourpet::Application.config.secret_token = 'c9958028fb8a60464da6960959c40220bb335d3ad2db9149022c5bed6820f26c75b9a7cd545e092947f09c73e24f85c688bbc5ba3d5cb945fe80ea659fbcf138'

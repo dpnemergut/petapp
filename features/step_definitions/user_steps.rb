2c431bb40a9b4411d00279245ab540804a83f156
Daniel Nemergut <dpnemergut@gmail.com>
2014-11-08 08:30:09 -0500

    Added sign in feature files and made some changes to them

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -61,3 +61,34 @@ end
 Then(/^I should see registration screen(\d+)$/) do |screen|
   visit 'users/registration screen1'
 end
+
+Given /^a user visits the signin page$/ do
+  visit 'users/login'
+end
+
+When /^he submits invalid signin information$/ do
+  click_button "commmit"
+end
+
+Then /^he should see an error message$/ do
+  page.should have_selector('div.alert.alert-error')
+end
+
+Given /^the user has an account$/ do
+  @user = User.create(:name => "Example User", :email => "user@example.com",
+  :password => "foobar", :password_confirmation => "foobar")
+end
+
+When /^the user submits valid signin information$/ do
+  fill_in "Email", :with => @user.email
+  fill_in "Password", :with => @user.password
+  click_button "commit"
+end
+
+Then /^he should see his profile page$/ do
+  page.should have_selector('title', :text => @user.name)
+end
+
+Then /^he should see a signout link$/ do
+  page.should have_link('Sign out', :href => signout_path)
+end

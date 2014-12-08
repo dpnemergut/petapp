11e404e01c39deb081be80fd9d63a2bd52ab216e
Mike Miller <dlsqueak@gmail.com>
2014-12-07 20:45:54 -0400

    added reset password form and functions

--- app/controllers/resetpassword_controller.rb
+++ app/controllers/resetpassword_controller.rb
@@ -1,6 +1,6 @@
 class ResetPasswordController < ApplicationController
   def checkQuestion
      question = User.get_security_question(params[:user][:username])
-     redirect_to :yourpet_pet_profile
+     
   end
 end

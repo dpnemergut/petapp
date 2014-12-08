c88d2a4f1b451727cf410cbe94aaeeaaf38d1e12
Daniel Nemergut <dpnemergut@gmail.com>
2014-12-08 18:39:52 -0500

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = [{:username => 'Santoshi', :email => 'santoshi.nitya@gmail.com', :password => 'hello',:petid => 'p01', :first_name => 'santoshi', :last_name => 'devata',:securityQuestion=>'What is my name', :securityAnswer=>'Santoshi', :password_confirmation =>'hello'},
-         {:username => 'Gayathri', :password => 'signal1', :petid => 'p02', :password_confirmation =>'signal1'},
+users = [{:username => 'Santoshi', :email => 'santoshi.nitya@gmail.com', :password => 'hello',:petid => 1, :first_name => 'santoshi', :last_name => 'devata',:securityQuestion=>'What is my name', :securityAnswer=>'Santoshi', :password_confirmation =>'hello'},
+         {:username => 'Gayathri', :password => 'signal1', :petid => 2, :password_confirmation =>'signal1'},
   	 ]
 
-pets = [{:petid => 'p01', :petname => 'Django', :breed => 'labrador', :color => 'grey', :dob => '2014-01-14', :interest => 'Stroll', :description => 'Puppy', :ownerfname => 'Santoshi', :ownerlname => 'Nithya', :gender => 'Male'},
-        {:petid => 'p02', :petname => 'Tiger', :breed => 'labrador', :color => 'white', :dob => '2014-03-14', :interest => 'Stroll', :description => 'Puppy', :ownerfname => 'Gayathri', :ownerlname => 'Sarathy', :gender => 'Male'}
+pets = [{:id => 1, :petname => 'Django', :breed => 'labrador', :color => 'grey', :dob => '2014-01-14', :interest => 'Stroll', :description => 'Puppy', :ownerfname => 'Santoshi', :ownerlname => 'Nithya', :gender => 'Male'},
+        {:id => 2, :petname => 'Tiger', :breed => 'labrador', :color => 'white', :dob => '2014-03-14', :interest => 'Stroll', :description => 'Puppy', :ownerfname => 'Gayathri', :ownerlname => 'Sarathy', :gender => 'Male'}
 			]
-poms = [{:petid => 'p01'},]
+poms = [{:id => 1 },]
 
 users.each do |user|
   User.create!(user)
